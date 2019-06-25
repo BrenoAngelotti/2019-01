1ae88ea6632f88ef486f37edfe00d43a546b1653
Breno Angelotti <brenogangelotti@gmail.com>
2019-06-24 23:39:57 -0300

    tratamento da seleção de país no método PUT

--- lesson-25/back-end/src/main/java/br/edu/utfpr/servico/ServicoCliente.java
+++ lesson-25/back-end/src/main/java/br/edu/utfpr/servico/ServicoCliente.java
@@ -1,23 +1,15 @@
 package br.edu.utfpr.servico;
 
+import br.edu.utfpr.dto.ClienteDTO;
+import br.edu.utfpr.dto.PaisDTO;
+import br.edu.utfpr.excecao.NomeClienteMenor5CaracteresException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import br.edu.utfpr.dao.Cliente;
-import br.edu.utfpr.dto.ClienteDTO;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.edu.utfpr.dto.PaisDTO;
-import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @RestController
 public class ServicoCliente {
@@ -37,7 +29,6 @@ public class ServicoCliente {
                 ClienteDTO.builder().id(1).nome("Breno Angelotti").idade(26).limiteCredito(3000.00).telefone("17 98814-3234").pais(paises.get(1)).build(),
                 ClienteDTO.builder().id(2).nome("Vivian Floriano").idade(22).limiteCredito(2000.00).telefone("43 98544-0185").pais(paises.get(0)).build(),
                 ClienteDTO.builder().id(3).nome("Bruna Angelotti").idade(22).limiteCredito(1000.00).telefone("17 98101-5311").pais(paises.get(0)).build()
-                //PaisDTO.builder().id(3).nome("Reino Unido").sigla("RU").codigoTelefone(44).build()
         ).collect(Collectors.toList());
     }
 
@@ -79,10 +70,15 @@ public class ServicoCliente {
         Optional<ClienteDTO> clienteExistente = clientes.stream().filter(p -> p.getId() == id).findAny();
 
         clienteExistente.ifPresent(c -> {
-            //c.setNome(cliente.getNome());
+            try {
+                c.setNome(cliente.getNome());
+            } catch (NomeClienteMenor5CaracteresException e) {
+                e.printStackTrace();
+            }
             c.setIdade(cliente.getIdade());
             c.setLimiteCredito(cliente.getLimiteCredito());
             c.setTelefone(cliente.getTelefone());
+            c.setPais(paises.stream().filter(p -> p.getId() == cliente.getPais().getId()).findAny().get());
         });
 
         return ResponseEntity.of(clienteExistente);
